c9d933fe470a7b8a0cfcd0665049fc8f9b98260d
Xinran Che <gracexinran@gmail.com>
2019-05-23 03:45:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,21 @@
 def consolidate_cart(cart)
   # code here
+  list = {}
+  cart.each do |x|
+    x.each do |k,v|
+      if !list[k]
+        list[k] = {}
+        v.each do |a,b|
+          list[k][a] = b
+        end
+        list[k][:count] = 1
+      else
+        list[k][:count] += 1
+      end
+    end
+  end
+  list
+
 end
 
 def apply_coupons(cart, coupons)
