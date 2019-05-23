5c5d0660f11cd08ea984c27fb40e5851d87a18c3
Xinran Che <gracexinran@gmail.com>
2019-05-23 04:20:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,7 +21,7 @@ end
 def apply_coupons(cart, coupons)
   # code here
   cart = consolidate_cart(cart)
-  
+
   cart.each do |name, value|
 
     if name == coupons[:item]
@@ -32,7 +32,7 @@ def apply_coupons(cart, coupons)
       cart[coupon][:clearance] = true
       cart[coupon][:count] = coupons[:num]
     end
-  
+
 
 end
   cart
